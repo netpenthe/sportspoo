19d816de3cca1eebfd50ba1d4a0b6650ae2ec80c
nai2 <nai2@nai2s-MacBook.local>
2013-01-03 23:10:24 +1030

    pinnacle no draws + fix queries

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -11,11 +11,12 @@ class FrontController < ApplicationController
    
     @league_events = Event.find_by_sql ["select events.* from events join leagues on leagues.id = events.league_id
       join user_preferences on (user_preferences.preference_id = league_id and preference_type='League')
-      where user_id=?",current_user.id]
+      where user_id=? and start_date > ? and start_date < ?", current_user.id,Time.now,Time.now+3.days]
     
     #@events = Event.where(["(league_id = 18 or league_id=6 or league_id=38 or league_id=2) 
     #                         and start_date > ? and start_date < ?", Time.now,Time.now+3.days]).order("start_date ASC")
     @events = @league_events
+    @events.sort!{|x,y| x.start_date <=> y.start_date}
   end
   
   def list_username    
