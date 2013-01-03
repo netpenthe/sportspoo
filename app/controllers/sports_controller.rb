6c177c10390711ee263a4ad6b134003826b5771a
nai2 <nai2@nai2s-MacBook.local>
2013-01-03 23:10:24 +1030

    pinnacle no draws + fix queries

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,7 +1,7 @@
 class SportsController < ApplicationController
 
   def index
-     @sports = Sport.includes(:leagues => :events).where(["events.start_date < ?", 4.days.ago])
+     @sports = Sport.includes(:leagues => :events).where(["events.start_date < ?", Time.now + 4.days])
      respond_to do |format|
       format.json { render json: @sports, :include => { :leagues => {:include => :events }  }}
      end
