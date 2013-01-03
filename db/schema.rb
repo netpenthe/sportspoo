cc2ec5bad65c1d21c8fc9ca70afd15917fcd57b8
nai2 <nai2@nai2s-MacBook.local>
2013-01-03 23:35:13 +1030

    countries and leagues db tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130101064854) do
+ActiveRecord::Schema.define(:version => 20130103130035) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(:version => 20130101064854) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "countries", :force => true do |t|
+    t.string   "name"
+    t.string   "code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "country_leagues", :id => false, :force => true do |t|
+    t.integer "country_id"
+    t.integer "league_id"
+  end
 
   create_table "events", :force => true do |t|
     t.integer  "sport_id"
