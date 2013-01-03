027cbb2d7b0a203f0deef0e019d6e6423ea020d2
nai2 <nai2@nai2s-MacBook.local>
2013-01-03 22:57:42 +1030

    leagues for a user

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -8,9 +8,14 @@ class FrontController < ApplicationController
     # select events.id,start_date from events join leagues on leagues.id = events.league_id
     #  join user_preferences on (user_preferences.preference_id = league_id and preference_type='League')
     #  where user_id=1;
+   
+    @league_events = Event.find_by_sql ["select events.* from events join leagues on leagues.id = events.league_id
+      join user_preferences on (user_preferences.preference_id = league_id and preference_type='League')
+      where user_id=?",current_user.id]
     
-    @events = Event.where(["(league_id = 18 or league_id=6 or league_id=38 or league_id=2) 
-                             and start_date > ? and start_date < ?", Time.now,Time.now+3.days]).order("start_date ASC")
+    #@events = Event.where(["(league_id = 18 or league_id=6 or league_id=38 or league_id=2) 
+    #                         and start_date > ? and start_date < ?", Time.now,Time.now+3.days]).order("start_date ASC")
+    @events = @league_events
   end
   
   def list_username    
