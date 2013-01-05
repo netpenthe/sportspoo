b1483465d01ab61ef66d96ec993a4930f630c19d
nai2 <nai2@nai2s-MacBook.local>
2013-01-05 15:50:33 +1030

    configure admin pages so they look nice

--- app/admin/countryleagues.rb
+++ app/admin/countryleagues.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Countryleague do
+
+  index do
+    selectable_column
+    column :country
+    column :league
+    default_actions
+  end
+
   
 end
--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Event do
+
+  index do
+    selectable_column
+    id_column
+    column :home_team
+    column :away_team
+    column :start_date
+    column :league
+    column :sport
+    default_actions
+  end
   
 end
