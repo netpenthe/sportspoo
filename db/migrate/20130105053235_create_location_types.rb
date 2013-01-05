a004b5bcc4c42b720f2cad645aad08ad6b4c056c
nai2 <nai2@nai2s-MacBook.local>
2013-01-05 16:59:14 +1030

    seed data

--- db/migrate/20130105053235_create_location_types.rb
+++ db/migrate/20130105053235_create_location_types.rb
@@ -1,9 +1,13 @@
 class CreateLocationTypes < ActiveRecord::Migration
   def change
+
     create_table :location_types do |t|
       t.string :name
-
       t.timestamps
     end
+
+    LocationType.create(:name=>"home")
+    LocationType.create(:name=>"away")
+   
   end
 end
