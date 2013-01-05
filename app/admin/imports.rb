b02811ba8984afd6f7fc06520cbf33e351e6e7dc
nai2 <nai2@nai2s-MacBook.local>
2013-01-05 20:49:23 +1030

    tweaks

--- app/admin/imports.rb
+++ app/admin/imports.rb
@@ -2,6 +2,15 @@ ActiveAdmin.register Import do
 
    #form :partial => "admin/form"
 
+  index do
+    selectable_column
+    id_column
+    column :sport_name
+    column :league_name
+    column :ics_file_name
+    default_actions
+  end
+
    form do |f|
       f.inputs "Details" do
         f.input :league_name
