b4d485d56f6c529839cc7daa9615bbfbae880cc0
nai2 <nai2@nai2s-MacBook.local>
2013-01-09 00:10:32 +1030

    tweak ics import

--- app/models/import_event.rb
+++ app/models/import_event.rb
@@ -1,3 +1,19 @@
 class ImportEvent < ActiveRecord::Base
   attr_accessible :dtend, :dtstart, :location, :summary, :import_id
+
+  belongs_to :import
+
+  def summary_filtered
+    unless import.filter_out_summary.blank?
+                   parts = import.filter_out_summary.split(",")
+                      if parts.count<2
+                        summary.gsub!(import.filter_out_summary,"")
+                      else
+                        parts.each do |part|
+                          summary.gsub!(part,"")
+                        end
+                      end
+    end
+    summary
+  end
 end
