f0b340b84e3aa887166dc70aad09701a56029407
Jon S <jon@jojati.com>
2013-01-09 22:44:59 +1030

    progressive

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,10 @@ class Country < ActiveRecord::Base
 
   attr_accessible :code, :name
 
+  #has_many :leagues, :through=>:countryleagues, :order=>"name DESC"
   has_many :leagues, :through=>:countryleagues, :order=>"name DESC"
+  has_many :events, :through=>:leagues
+  has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=>['start_date > ?', Time.now.utc - 1.hour], :order=>'start_date'
   has_many :countryleagues
 
 end
