aa078e04f8693a96100a13b6d39985008442b96d
nai2 <nai2@nai2s-MacBook.local>
2013-01-13 00:51:48 +1030

    fail

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,5 +1,5 @@
 class Import < ActiveRecord::Base
-  attr_accessible :filter_out_summary, :league_name, :split_summary_on, :sport_name
+  attr_accessible :filter_out_summary, :league_name, :split_summary_on, :sport_name,:home_team_first
 
   attr_accessible :ics
   has_attached_file :ics
