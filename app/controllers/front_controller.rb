c1ce33e393b18b3f154c0ad65b2908484b8a33c7
nai2 <nai2@nai2s-MacBook.local>
2013-01-13 22:44:54 +1030

    fail

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -9,7 +9,7 @@ class FrontController < ApplicationController
     #  join user_preferences on (user_preferences.preference_id = league_id and preference_type='League')
     #  where user_id=1;
     user = current_user unless current_user.blank?
-    user = User.find_by_name params[:username] if user.blank?
+    user = User.find_by_username params[:username] if user.blank?
   
     @team_events = Array.new
     @league_events = Array.new
@@ -17,7 +17,7 @@ class FrontController < ApplicationController
    
     @league_events = Event.find_by_sql ["select events.* from events join leagues on leagues.id = events.league_id
       join user_preferences on (user_preferences.preference_id = league_id and preference_type='League')
-      where user_id=? and start_date > ? and start_date < ?", current_user.id,Time.now,Time.now+7.days]
+      where user_id=? and start_date > ? and start_date < ?", user.id,Time.now,Time.now+7.days]
 
     @sport_events = Event.find_by_sql ["select events.* from events join sports on sports.id = events.sport_id
       join user_preferences on (user_preferences.preference_id = sport_id and preference_type='Sport')
@@ -25,7 +25,7 @@ class FrontController < ApplicationController
 
     #@team_events = Event.find_by_sql ["select events.* from events join teams on (teams.id = events.home_team_id or teams.id = events.away_team_id)
     #  join user_preferences on ((user_preferences.preference_id = home_team_id and preference_type='Team') or (user_preferences.preference_id = away_team_id and preference_type='Team'))
-    #  where user_id=? and start_date > ? and start_date < ?", current_user.id,Time.now,Time.now+7.days]
+    #  where user_id=? and start_date > ? and start_date < ?", user.id,Time.now,Time.now+7.days]
     
     @events = @league_events + @sport_events + @team_events
 
