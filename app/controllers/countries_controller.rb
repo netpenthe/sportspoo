105291e4661b58f4d1e3b1b89a1c3d67902f1231
nai2 <nai2@nai2s-MacBook.local>
2013-02-07 23:30:38 +1030

    add priority

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,27 +1,13 @@
 class CountriesController < ApplicationController
 
-  def xleagues 
+  def leagues 
      @country = Country.find_by_name params[:country]
 
      respond_to do |format|
       unless params[:events].blank?
         format.json { render json: @country.leagues }
       else
-        format.json { render json: @country.leagues, :include => { :events => {:include => :teams}}}
-      end
-     end
-  end
-
-
-  def leagues
-     @country = Country.find_by_name params[:country]
-
-     respond_to do |format|
-      #format.json { render json: @country.upcoming_events}
-      unless params[:events].blank?
-        format.json { render json: @country.leagues }
-      else
-        format.json { render json: @country.upcoming_events, :include => [:teams]}
+        format.json { render json: @country, :include => {  :countryleagues => {:include => {:league => { :include=> { :events => { :include=> :teams}}}}}}}
       end
      end
   end
