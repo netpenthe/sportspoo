38d88c8cf1f104b0b71bdf956c0e524d54b1a9ac
nai2 <nai2@nai2s-MacBook.local>
2013-02-11 22:40:48 +1030

    priority 0 only

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,7 +4,7 @@ class CountriesController < ApplicationController
      @country = Country.find_by_name params[:country]
 
      respond_to do |format|
-        format.json { render json: @country.leagues, :params=>{:country_id => @country.id}}
+        format.json { render json: @country.leagues.where("priority=0"), :params=>{:country_id => @country.id}}
      end
   end
 
