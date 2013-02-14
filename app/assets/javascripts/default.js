6c3967a681c42edb033a79b97e9bd28f88e2b058
Jon S <jon@jojati.com>
2013-02-14 21:53:15 +1030

    trees and sheet

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,47 +1,68 @@
-/* start geolocate stuff */
-var geocoder;
-var city;
-var latitude;
-var longitude;
-var country = "UNITED STATES";
 
-//Get the latitude and the longitude;
-function successFunction(position) {
+var locator =  function() {
+  this.city = '';
+  this.latitude = '';
+  this.longitude = '';
+  this.country = "UNITED STATES";
+  this.sports_ui = '';
+  this.geocoder = new google.maps.Geocoder();
+};
+/* MOVED TO BE ANONYMOUS FUNCTION 
+locator.prototype.successFunction = function(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  codeLatLng(latitude, longitude)
+
+  myLocator.codeLatLng(latitude, longitude); // this should not call myLocator, it should be 'this' but the problem is this 'successFunction' is a CallBack and doesn't seem to send the whole object
+};
+*/
+
+locator.prototype.codeLatLng = function(lat, lng) {
+  var latlng = new google.maps.LatLng(lat, lng);
+  var me = this;
+  this.geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      console.log(results)
+      if (results[1]) {
+      //formatted address
+      me.city = results[results.length-2].formatted_address;
+      me.country = results[results.length-1].formatted_address;
+      me.locationFound();
+      } else {
+      me.locationNotFound();
+      }
+      } else {
+      me.locationNotFound();
+      //alert("Geocoder failed due to: " + status);
+      }
+      });
+};
+
+locator.prototype.successFunctionL = function() {
+  alert("what?");
+  this.codeLatLng(1,2);
 }
 
-function codeLatLng(lat, lng) {
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        console.log(results)
-        if (results[1]) {
-          //formatted address
-          city = results[results.length-2].formatted_address;
-          country = results[results.length-1].formatted_address;
-          locationFound();
-        } else {
-          locationNotFound();
-        }
-      } else {
-        locationNotFound();
-        //alert("Geocoder failed due to: " + status);
-      }
-  });
+locator.prototype.getMail = function () {  
+  //this is a function on new instances of that object
+  //whatever code you like
+  return mail;
 }
-geocoder = new google.maps.Geocoder();
+
+locator.prototype.otherMethod = function () { 
+  //this is another function that can access obj.getMail via 'this'
+  this.getMail();
+}
+//geocoder = new google.maps.Geocoder();
 /* end geolocate stuff */
 
-function locationFound() {
+locator.prototype.locationFound = function() {
   /* As soon as locationFound, update left and middle columns */
-  updateEvents(country);
-  updateTree(); 
+  this.sports_ui.updateEvents(this.country);
+  this.sports_ui.updateTree(this.country); 
 
   //var mins = TimeZoneDetect();
   //var mins = new Date().getTimezoneOffset();
-  var mins = TimeZoneDetect();
+  var mins = this.TimeZoneDetect();
   var hours = Math.abs(Math.round( mins / 60));        
   var minutes = Math.abs(mins % 60);
   var offset;
@@ -53,30 +74,30 @@ function locationFound() {
   }
 
   var timezone = jstz.determine();
-  tz = timezone.name(); 
-  DST = DSTActive() ? " - Daylight Savings" : "";
-  $('#location_guess').html("We think you're in "+city+" ("+tz+" GMT"+offset+DST+")");
+  var tz = timezone.name(); 
+  DST = this.DSTActive() ? " - Daylight Savings" : "";
+  $('#location_guess').html("We think you're in "+this.city+" ("+tz+" GMT"+offset+DST+")");
 
   $('#user_time_zone').val(tz.split("/")[1]);
 
   //$('#location_guess').html("Timezone: "+tz+" (GMT"+offset+")");
-  
+
   /*$.getJSON('https://maps.googleapis.com/maps/api/timezone/json?location='+latitude+','+longitude+'&timestamp=1331161200&sensor=false',function(j){
     $('#location_guess').html("We think you're in "+city+" ("+j.timeZoneId+")");
-  });
-$.ajax({
+    });
+    $.ajax({
 
-    url: 'https://maps.googleapis.com/maps/api/timezone/json?location='+latitude+','+longitude+'&timestamp=1331161200&sensor=false',
-    type: 'GET',
-    crossDomain: true,
-    dataType: 'jsonp',
-    success: function() { alert("Success"); },
-    error: function() { alert('Failed!'); },
+url: 'https://maps.googleapis.com/maps/api/timezone/json?location='+latitude+','+longitude+'&timestamp=1331161200&sensor=false',
+type: 'GET',
+crossDomain: true,
+dataType: 'jsonp',
+success: function() { alert("Success"); },
+error: function() { alert('Failed!'); },
 });
 */
-}
+  };
 
-function DSTActive() {
+locator.prototype.DSTActive = function() {
   var arr = [];
   for (var i = 0; i < 365; i++) {
     var d = new Date();
@@ -87,34 +108,35 @@ function DSTActive() {
   DST = Math.min.apply(null, arr);
   nonDST = Math.max.apply(null, arr);
   return (DST != nonDST);
-}
+};
 
-function locationNotFound() {
+locator.prototype.locationNotFound = function() {
   $('#location_guess').html("We can't find you. We'll put you in New York");
-}
-function TimeZoneDetect(){
-    var dtDate = new Date('1/1/' + (new Date()).getUTCFullYear());
-    var intOffset = 10000; //set initial offset high so it is adjusted on the first attempt
-    var intMonth;
-    var intHoursUtc;
-    var intHours;
-    var intDaysMultiplyBy;
- 
-    //go through each month to find the lowest offset to account for DST
-    for (intMonth=0;intMonth < 12;intMonth++){
-        //go to the next month
-        dtDate.setUTCMonth(dtDate.getUTCMonth() + 1);
- 
-        //To ignore daylight saving time look for the lowest offset.
-        //Since, during DST, the clock moves forward, it'll be a bigger number.
-        /*if (intOffset > (dtDate.getTimezoneOffset() * (-1))){
-            intOffset = (dtDate.getTimezoneOffset() * (-1));
-        }*/
-        if (!intOffset || intOffset > (dtDate.getTimezoneOffset() * (-1))){
-          intOffset = (dtDate.getTimezoneOffset() * (-1));
-        }
+};
+
+locator.prototype.TimeZoneDetect = function(){
+  var dtDate = new Date('1/1/' + (new Date()).getUTCFullYear());
+  var intOffset = 10000; //set initial offset high so it is adjusted on the first attempt
+  var intMonth;
+  var intHoursUtc;
+  var intHours;
+  var intDaysMultiplyBy;
+
+  //go through each month to find the lowest offset to account for DST
+  for (intMonth=0;intMonth < 12;intMonth++){
+    //go to the next month
+    dtDate.setUTCMonth(dtDate.getUTCMonth() + 1);
+
+    //To ignore daylight saving time look for the lowest offset.
+    //Since, during DST, the clock moves forward, it'll be a bigger number.
+    /*if (intOffset > (dtDate.getTimezoneOffset() * (-1))){
+      intOffset = (dtDate.getTimezoneOffset() * (-1));
+      }*/
+    if (!intOffset || intOffset > (dtDate.getTimezoneOffset() * (-1))){
+      intOffset = (dtDate.getTimezoneOffset() * (-1));
     }
- 
-    return intOffset;
+  }
+
+  return intOffset;
 }
 
--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -1,134 +1,176 @@
-
+var myLocator;
+var mySportsUI;
 // This is where all the page initialisation happens.
 $(document).ready(function () {
-  // first locate
-  if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(successFunction, locationNotFound); } 
-  
-  // then fill out left menu 
+    // first locate
+    //mySportsUI = Object.create(sports_ui);
+    mySportsUI = new sports_ui;
+    myLocator = new locator;
+    myLocator.sports_ui = mySportsUI; 
+    if (navigator.geolocation) { 
+      navigator.geolocation.getCurrentPosition(
+      function(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        myLocator.codeLatLng(latitude, longitude); // this should not call myLocator, it should be 'this' but the problem is this 'successFunction' is a CallBack and doesn't seem to send the whole object
+      },
+      function() {
+        $('#location_guess').html("We can't find you. We'll put you in New York");
+      }
+    );
+    }
 
-  // then fill out center menu
-});
 
+    // then fill out left menu 
 
-function updateEvents(country) {
+    // then fill out center menu
+    });
+
+var sports_ui = function() {
+  this.leagues_jsons = {} 
+};
+
+sports_ui.prototype.updateEvents = function(country) {
   $.getJSON( '/country/events/'+country+'.json',
-  { },
-  function(events) {
-    var start_date_utc, start_date_local;
-    //for (i=0;i<leagues.length;i++) {
+      { },
+      function(events) {
+      var start_date_utc, start_date_local;
+      //for (i=0;i<leagues.length;i++) {
       // l =  leagues[i];
       for (j=0;j<events.length;j++) {
-        e = events[j];
-        if (e.teams.length==2) {
-          start_date_utc = new Date(e.start_date);
-          start_date_local = start_date_utc.toString('ddd hh:mmtt'); 
-          $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_id_'+e.league_id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' vs ' + e.teams[1].name +' </li>')
-          //            $('#events').append(j+"  :"+e.league.name + " - ");  
-          //           $('#events').append("  >>"+e.teams[0].name + " vs " + e.teams[1].name );  
-          //          $('#events').append(" start: "+e.start_date+"<br />");  
-          }
-        }
-        //}
+      e = events[j];
+      if (e.teams.length==2) {
+      start_date_utc = new Date(e.start_date);
+      start_date_local = start_date_utc.toString('ddd hh:mmtt'); 
+      $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_id_'+e.league_id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' vs ' + e.teams[1].name +' </li>')
+      //            $('#events').append(j+"  :"+e.league.name + " - ");  
+      //           $('#events').append("  >>"+e.teams[0].name + " vs " + e.teams[1].name );  
+      //          $('#events').append(" start: "+e.start_date+"<br />");  
+      }
+      }
+      //}
       //$.mobile.loading('hide');
-    }); 
-}
+  }); 
+};
+sports_ui.prototype.displayEventsForLeague =  function(events) {
+  // cache it if it hasn't been cahsed before
+  if (events.length > 0) {
+    if (!this.leagues_jsons.hasOwnProperty("L"+events[0].league_id)) {
+      this.leagues_jsons["L"+events[0].league_id] = events;
+    }
+  }
 
-function getEvents(cb,league_id) {
-  if (!cb.checked) { // hide all events for this league
-    $('.league_id_'+league_id).fadeOut(1000);
-    //$('.league_id_'+league_id).remove();
-  } else {
-    if ($(".league_id_"+league_id).length > 0) {
-      $('.league_id_'+league_id).show().effect("highlight",{},1500);
-    } else {
-    
-    $.getJSON( '/leagues/events/'+league_id+'.json',
-    { },
-    function(events) {
-      var start_date_utc, start_date_local, timestamp;
-      for (var i=0;i<events.length;i++) {
-        e = events[i];
-        if (e.teams.length==2) {
-          start_date_utc = new Date(e.start_date);
-          start_date_local = start_date_utc.toString('ddd hh:mmtt'); 
-          timestamp = start_date_utc.getTime();
+  var start_date_utc, start_date_local, timestamp;
+  for (var i=0;i<events.length;i++) {
+    e = events[i];
+    if (e.teams.length==2) {
+      start_date_utc = new Date(e.start_date);
+      start_date_local = start_date_utc.toString('ddd hh:mmtt'); 
+      timestamp = start_date_utc.getTime();
 
-/*
-        $('#list1 li').each(function(index) {
-          var foo = $('#list1 li')[index];
-          if (parseInt(foo.getAttribute("timestamp")) <= timestamp) {
-            if (parseInt(foo.getAttribute("timestamp")) <= timestamp) {
-              $("#list1").append('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
-            }
-            //$("#list1 li")[j].insertBefore('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
-            //break;
-          }
- 
-        });*/
-        
-         for (var j = $('#list1 li').length-1;j>=0;j--) {
-          var list_item = $('#list1 li')[j];
-          if (parseInt(list_item.getAttribute("timestamp")) <= timestamp) {
-            var new_event = '<li class="ui-li ui-li-static ui-btn-up-c league_id_'+league_id+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + '  vs ' + e.teams[1].name +' </li>';
-            $(new_event).hide().insertAfter($("#list1 li:nth-child("+j+")")).effect("highlight", {}, 1500);;
-            //$('<li class="ui-li ui-li-static ui-btn-up-c league_id_'+league_id+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + '  vs ' + e.teams[1].name +' </li>').hide().insertAfter($("#list1 li:nth-child("+j+")")).effect("highlight", {}, 1500);;
-            //$("#list1 li:nth-child("+j+")").insertAfter('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
-            //$("#list1 li")[j].insertBefore('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
-            break;
-          }
+      /*
+         $('#list1 li').each(function(index) {
+         var foo = $('#list1 li')[index];
+         if (parseInt(foo.getAttribute("timestamp")) <= timestamp) {
+         if (parseInt(foo.getAttribute("timestamp")) <= timestamp) {
+         $("#list1").append('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
          }
+      //$("#list1 li")[j].insertBefore('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
+      //break;
+      }
+
+      });*/
+
+      var new_event;
+      new_event = '<li class="ui-li ui-li-static ui-btn-up-c league_id_'+e.league_id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + '  vs ' + e.teams[1].name +' </li>';
+
+      if ($('#list1 li').length ==0) {
+        //$(new_event).hide().append($("#list1")).effect("highlight", {}, 1500);;
+        $('#list1').append(new_event).hide().effect("highlight", {},1500);
+      } else {
+      for (var j = $('#list1 li').length-1;j>=0;j--) {
+        var list_item = $('#list1 li')[j];
+        if (parseInt(list_item.getAttribute("timestamp")) <= timestamp) {
+          //new_event = '<li class="ui-li ui-li-static ui-btn-up-c league_id_'+e.league_id+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + '  vs ' + e.teams[1].name +' </li>';
+    
+            $(new_event).hide().insertAfter($("#list1 li:nth-child("+(j+1)+")")).effect("highlight", {}, 1500);;
+
+          //$('<li class="ui-li ui-li-static ui-btn-up-c league_id_'+league_id+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + '  vs ' + e.teams[1].name +' </li>').hide().insertAfter($("#list1 li:nth-child("+j+")")).effect("highlight", {}, 1500);;
+          //$("#list1 li:nth-child("+j+")").insertAfter('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
+          //$("#list1 li")[j].insertBefore('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup>'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' !!!!vs ' + e.teams[1].name +' </li>')
+          break;
         }
       }
-    });
+    }
     }
   }
 }
 
-function updateTree() {
-  $.getJSON( '/country/leagues/'+country+'.json',
-  { },
-  function(leagues) {
-    var theres_more = false;
-    for (i=0;i<leagues.length;i++) {
-      if (i ==0 ) { 
-        $('#main_sports_list').append('<h5>'+leagues[i].sport+'</h5>');
-        $('#main_sports_list').append('<ul>');
-      } else {
-        if (leagues[i].sport != leagues[i-1].sport) {
-          if (theres_more) {
-            $('#main_sports_list').append('<li><a href="#" onclick="$(\'.tree_league_id_'+leagues[i-1].sport_id+'\').fadeIn()">show more...</a></li>');
-            theres_more = false;
+sports_ui.prototype.getEvents = function(cb,league_id) {
+      var me = this;
+  if (!cb.checked) { // hide all events for this league
+    $('.league_id_'+league_id).fadeOut(1000);
+    setTimeout(function(){$('.league_id_'+league_id).remove()}, 1000);
+  } else {
+    if (this.leagues_jsons.hasOwnProperty("L"+league_id)) {
+      //$('.league_id_'+league_id).show().effect("highlight",{},1500);
+      this.displayEventsForLeague(this.leagues_jsons["L"+league_id]);
+    } else {
+      $.getJSON( '/leagues/events/'+league_id+'.json',
+          { },
+          function (events) {
+            me.displayEventsForLeague(events)
           }
-          $('#main_sports_list').append('</ul>');
-          $('#main_sports_list').append('<h5>'+leagues[i].sport+'</h5>');
-          $('#main_sports_list').append('<ul>');
-        }
+      );
+    }
+  }
+};
+
+sports_ui.prototype.updateTree = function(country) {
+  $.getJSON( '/country/leagues/'+country+'.json',
+      { },
+      function(leagues) {
+      var theres_more = false;
+      for (i=0;i<leagues.length;i++) {
+      if (i ==0 ) { 
+      $('#main_sports_list').append('<h5>'+leagues[i].sport+'</h5>');
+      $('#main_sports_list').append('<ul>');
+      } else {
+      if (leagues[i].sport != leagues[i-1].sport) {
+      if (theres_more) {
+      $('#main_sports_list').append('<li><a href="#" onclick="$(\'.tree_league_id_'+leagues[i-1].sport_id+'\').fadeIn()">show more...</a></li>');
+      theres_more = false;
+      }
+      $('#main_sports_list').append('</ul>');
+      $('#main_sports_list').append('<h5>'+leagues[i].sport+'</h5>');
+      $('#main_sports_list').append('<ul>');
+      }
       }
 
       switch (leagues[i].priority) {
         case 1:
-          $('#main_sports_list').append('<li>'+leagues[i].name+'<input type=checkbox align=right onclick="getEvents(this, '+leagues[i].id+')"></li>');
+          $('#main_sports_list').append('<li>'+leagues[i].name+'<input type=checkbox align=right onclick="mySportsUI.getEvents(this, '+leagues[i].id+')"></li>');
           break;
         case 2:
           theres_more = true;
-          $('#main_sports_list').append('<li class="league_hidden tree_league_id_'+leagues[i].sport_id+'">'+leagues[i].name+'<input type=checkbox id='+leagues[i].sport_id+' onclick="getEvents(this,'+leagues[i].id+')" align=right></li>');
+          $('#main_sports_list').append('<li class="league_hidden tree_league_id_'+leagues[i].sport_id+'">'+leagues[i].name+'<input type=checkbox id='+leagues[i].sport_id+' onclick="mySportsUI.getEvents(this,'+leagues[i].id+')" align=right></li>');
           break;
         default:
-          $('#main_sports_list').append('<li>'+leagues[i].name+'<input type=checkbox onclick="getEvents(this, '+leagues[i].id+')" checked align=right></li>');
+          $('#main_sports_list').append('<li>'+leagues[i].name+'<input type=checkbox onclick="mySportsUI.getEvents(this, '+leagues[i].id+')" checked align=right></li>');
           break;
       }
 
-          //$("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong></p>'+e.league+' - '+e.teams[0].name + ' vs ' + e.teams[1].name +' </li>')
-          //            $('#events').append(j+"  :"+e.league.name + " - ");  
-          //           $('#events').append("  >>"+e.teams[0].name + " vs " + e.teams[1].name );  
-          //          $('#events').append(" start: "+e.start_date+"<br />");  
-     }
-     $('#main_sports_list').append('</ul>');
-        //}
-    }); 
-}
+      //$("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong></p>'+e.league+' - '+e.teams[0].name + ' vs ' + e.teams[1].name +' </li>')
+      //            $('#events').append(j+"  :"+e.league.name + " - ");  
+      //           $('#events').append("  >>"+e.teams[0].name + " vs " + e.teams[1].name );  
+      //          $('#events').append(" start: "+e.start_date+"<br />");  
+      }
+      $('#main_sports_list').append('</ul>');
+      //}
+  }); 
+}; 
 
-function localize(t)
+sports_ui.prototype.localize = function(t)
 {
   var d=new Date(t+" UTC");
   document.write(d.toString());
