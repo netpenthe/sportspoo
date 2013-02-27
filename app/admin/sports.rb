5d91d6a8192b0e05f0d254593982e1720d3231e4
nai2 <nai2@nai2s-MacBook.local>
2013-02-27 23:49:38 +1030

    nicknames / names for teams and sports

--- app/admin/sports.rb
+++ app/admin/sports.rb
@@ -1,11 +1,30 @@
 ActiveAdmin.register Sport do
 
-   index do
+  index do
     selectable_column
     id_column
     column :name
+    column :all_names
     default_actions
   end
 
+
+    form do |f|
+      
+      f.inputs "Details" do
+        f.input :name
+      end
+
+      f.inputs "Nicknames" do
+        f.input :nickname_list
+      end
+
+      f.inputs "Short Name" do
+        f.input :shortname_list
+      end
+
+      f.buttons
+    end
+ 
   
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,16 +2,24 @@ class Team < ActiveRecord::Base
 
   #check if team exists in this sport
   belongs_to :sport
+
+  acts_as_taggable
+  acts_as_taggable_on :nicknames, :shortnames
+  
   
   def self.find_for_sport name, sport_id
     Team.joins("join event_teams on event_teams.team_id = teams.id
                                    join events on events.id = event_teams.event_id").where(["events.sport_id = ? and teams.name = ?",sport_id,name]).first
   end
  
-    def as_json(options ={})
+  def as_json(options ={})
     h = super(options)
     h[:sport_name] = self.sport.present? ? self.sport.name : 'sprt' 
     h
+  end
+
+  def all_names
+    "#{shortname_list} | #{nickname_list}"
   end
 
   
