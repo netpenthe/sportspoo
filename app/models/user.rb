12f7383682ff5c6e07040d7cd337a35dfa6f0a03
nai2 <nai2@nai2s-MacBook.local>
2013-03-14 22:30:34 +1030

    allow existing users

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.extra.raw_info.name,
+      user = User.create(name:"",
                          provider:auth.provider,
                          uid:auth.uid,
                          email:auth.info.email,
