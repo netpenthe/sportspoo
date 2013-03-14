3f88ffd6d129866d354615791b7f391c4c923b59
nai2 <nai2@nai2s-MacBook.local>
2013-03-14 22:30:34 +1030

    allow existing users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,8 +17,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def twitter
-     session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-     render "users/twitter"
+    @user = User.find_for_twitter_oauth(request_data, current_user)
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
+      render "users/twitter"
+    end
   end
 
 
