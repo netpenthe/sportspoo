635505b32e42605a6dde539b1db86e6e56d4da32
nai2 <nai2@nai2s-MacBook.local>
2013-03-14 21:56:31 +1030

    twitter tweaks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,12 +17,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
   def twitter
-
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    request_data = request.env["omniauth.auth"]
+    
+    unless params[:email].blank?
+      request_data[:info][:email] = params[:email]
+    else
+      @user = User.find_for_twitter_oauth(request_data, current_user)
+    end
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    elsif !params[:email]
+      render "users/twitter"
     else
       session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
