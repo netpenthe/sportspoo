dd2a97e542465ea1897f292958248d7978b000b9
nai2 <nai2@nai2s-MacBook.local>
2013-03-14 23:04:39 +1030

    label colours

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -90,8 +90,9 @@ sports_ui.prototype.updateInitialEvents = function(country) {
             //start_date_local = start_date_utc.toString('ddd hh:mmtt'); 
             //start_date_local = moment(start_date_utc).format('ddd h:mma');
             start_date_local = "";
+            league_label_colour = e.league_label_colour;
             //$("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_event league_id_'+e.league_id+' T'+e.teams[0].id+' T'+e.teams[1].id+'" league_id="'+e.league_id+'" event_id="'+e.id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup >'+start_date_local+'</sup></p>'+e.league+' - '+e.teams[0].name + ' vs ' + e.teams[1].name +' </li>')
-            $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_event league_id_'+e.league_id+' T'+e.teams[0].id+' T'+e.teams[1].id+'" league_id="'+e.league_id+'" event_id="'+e.id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup >'+start_date_local+'</sup></p>'+e.teams[0].name + ' vs ' + e.teams[1].name + '  <span class="label labelstyle-FC9300" data-name="activesupport">'+e.league+'</span>'  + ' </li>')
+            $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_event league_id_'+e.league_id+' T'+e.teams[0].id+' T'+e.teams[1].id+'" league_id="'+e.league_id+'" event_id="'+e.id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+e.time_in_words+'</strong> <sup >'+start_date_local+'</sup></p>'+e.teams[0].name + ' vs ' + e.teams[1].name + '  <span class="label" style="background-color:'+ league_label_colour+'" data-name="activesupport">'+e.league+'</span>'  + ' </li>')
           }
         }
         //me.removeEvents(me.my_events);
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227125212) do
+ActiveRecord::Schema.define(:version => 20130314122154) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -51,11 +51,6 @@ ActiveRecord::Schema.define(:version => 20130227125212) do
     t.string   "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-  end
-
-  create_table "country_leagues", :id => false, :force => true do |t|
-    t.integer "country_id"
-    t.integer "league_id"
   end
 
   create_table "countryleagues", :force => true do |t|
@@ -125,6 +120,7 @@ ActiveRecord::Schema.define(:version => 20130227125212) do
     t.datetime "updated_at"
     t.integer  "import_id"
     t.integer  "sport_id"
+    t.string   "label_colour"
   end
 
   create_table "location_types", :force => true do |t|
