3cf8019e25c8bdee4e3d1b6e899eadaf938c1975
nai2 <nai2@nai2s-MacBook.local>
2013-03-16 18:28:56 +1030

    event tags for big games/derby

--- app/assets/javascripts/front.js
+++ app/assets/javascripts/front.js
@@ -107,11 +107,21 @@ sports_ui.prototype.updateInitialEvents = function(country) {
             //start_date_local = moment(start_date_utc).format('ddd h:mma');
             start_date_local = "";
             league_label_colour = e.league_label_colour;
+
+            // show live event label
             live_event_str = "  ";
             if(e.live){
               live_event_str = " <span class='label' style='background-color:green'>LIVE</span> ";
             }
-            $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_event league_id_'+e.league_id+teams_class+'" league_id="'+e.league_id+'" event_id="'+e.id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+time_in_words+'</strong> <sup >'+start_date_local+'</sup></p>'+display_name+ '  <span class="label" style="background-color:'+ league_label_colour+'" data-name="activesupport">'+e.league+'</span> '  + live_event_str + ' </li>')
+
+            //show event tags/label
+            var tag_str = "";
+            var t;
+            for (t = 0; t < e.tag_list.length; t++) {
+              tag_str = tag_str + " <span class='label' style='background-color:#0099cc'> " + e.tag_list[t] + "</span> ";
+            }
+
+            $("#list1 ").append('<li class="ui-li ui-li-static ui-btn-up-c league_event league_id_'+e.league_id+teams_class+'" league_id="'+e.league_id+'" event_id="'+e.id+'" timestamp="'+start_date_utc.getTime()+'"><p class="ui-li-aside ui-li-desc"><strong>'+time_in_words+'</strong> <sup >'+start_date_local+'</sup></p>'+display_name+ '  <span class="label" style="background-color:'+ league_label_colour+'" data-name="activesupport">'+e.league+'</span> '  + live_event_str + tag_str + ' </li>')
           }
         //me.removeEvents(me.my_events);
         me.displayEventsForLeague(me.my_events,"my_events");
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ActiveRecord::Base
 
-include ActionView::Helpers::DateHelper
-  
+  include ActionView::Helpers::DateHelper
+
+  acts_as_taggable
+
   has_many :home_teams, :class_name=>"Team", :through=>:event_teams, :source=>:team, :conditions=>"location_type_id = 1"
   has_many :away_teams, :class_name=>"Team", :through=>:event_teams, :source=>:team, :conditions=>"location_type_id = 2"
 
