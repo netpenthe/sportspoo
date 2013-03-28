49e4f04af2db117dc12cadb49b7fc76a628b418b
Jon S <jon@jojati.com>
2013-03-28 19:56:29 +1030

    more events on front page

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,7 +5,7 @@ class Country < ActiveRecord::Base
   #has_many :leagues, :through=>:countryleagues, :order=>"name DESC"
   has_many :leagues, :through=>:countryleagues, :order=>"sport_id, priority, name DESC"
   has_many :events, :through=>:leagues
-  has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now - 2.hour}' and priority=0"}, :order=>'start_date', :limit=>25
+  has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now - 2.hour}' and priority=0"}, :order=>'start_date', :limit=>100
 
   #has_many :boo,  lambda { where("created_at <= ?", Time.now) }
  #:order=>'start_date', :limit=>25
