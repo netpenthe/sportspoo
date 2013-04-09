f4f72db15f99144a5e9498ad62fd36b27baad1cf
Jon S <jon@jojati.com>
2013-04-09 21:22:22 +0930

    disable the javascrtip location detection

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -91,13 +91,10 @@ locator.prototype.locationFound = function() {
   var tz = timezone.name(); 
   DST = this.DSTActive() ? " - Daylight Savings" : "";
 
-  
-
  // $('#location_guess').html("We think you're in "+this.city+" ("+tz+" GMT"+offset+DST+")");
   this.sports_ui.set_tz_selector(tz.substring(tz.indexOf("/")+1), offset_no_dst);
   $('#user_time_zone').val(tz.split("/")[1]);
   mySportsUI.change_time_zone(offset_dec); 
-  
   
   //$('#location_guess').html("Timezone: "+tz+" (GMT"+offset+")");
 
