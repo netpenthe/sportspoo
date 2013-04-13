eb777a1a4b0fa810271326db8c5ac48dadcbb353
nai2 <nai2@nai2s-MacBook.local>
2013-04-13 22:27:33 +0930

    faile

--- app/models/event.rb
+++ app/models/event.rb
@@ -48,22 +48,12 @@ class Event < ActiveRecord::Base
 
 
   def as_json(options ={})
-    options[:except] << :import_id
-    options[:except] << :created_at
-    options[:except] << :import_event_id
+    options[:except] = [:import_id, :created_at,:import_event_id,:location_id,:updated_at]
     h = super(options)
     h[:league] = league.name
     h[:time_in_words] = distance_of_time_in_words_to_now self.start_date
     h[:running] = self.start_date < Time.now ? true : false
     h[:quick_datetime] = self.start_date.strftime('%a%l:%M%P')
-
-    #not used
-    h.delete(:import_id)
-    h.delete(:created_at)  
-    h.delete(:import_event_id)  
-    h.delete(:location_id)  
-    h.delete(:updated_at)  
-
     h
   end
 
