37608fa09ab6d6b23077afe4f62234bb7de8941c
Jon S <jon@jojati.com>
2013-04-17 23:18:16 +0930

    inifite load for non logged in

--- app/models/country.rb
+++ app/models/country.rb
@@ -6,11 +6,15 @@ class Country < ActiveRecord::Base
   has_many :leagues, :through=>:countryleagues, :order=>"sport_id, priority, name DESC"
   has_many :events, :through=>:leagues
   #has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now.utc - Constants::RUNNING_EVENTS_HOURS.hour}' and priority=0"}, :order=>'start_date', :limit=>30
-  has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now.utc - Constants::RUNNING_EVENTS_HOURS.hour}' and priority=0"}, :order=>'start_date', :limit=>30
 
   #has_many :boo,  lambda { where("created_at <= ?", Time.now) }
  #:order=>'start_date', :limit=>25
   #has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=>['start_date > ?', Time.now.utc - 1.hour], :order=>'start_date', :limit=>25
   has_many :countryleagues
 
+  has_many :upcoming_events, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now.utc - Constants::RUNNING_EVENTS_HOURS.hour}' and priority=0"}, :order=>'start_date', :limit=>30
+  has_many :upcoming_events_moar, :source => :events, :through=>:leagues, :conditions=> proc {"start_date > '#{Time.now.utc - Constants::RUNNING_EVENTS_HOURS.hour}' and priority=0"}, :order=>'start_date', :limit=>3000
+
 end
+
+
