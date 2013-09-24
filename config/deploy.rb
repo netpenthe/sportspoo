434c5107a4d22ae776976af35dfb908e51e0f2f8
nai2 <adnaismi@yahoo.com>
2013-09-24 19:23:05 +0930

    ffs

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,8 @@ set :keep_releases, 5
 
 
 set :application, "sportspoo"
-set :repository,  "git@bitbucket.org:desmondy/sportspoo.git"
+#set :repository,  "git@bitbucket.org:desmondy/sportspoo.git"
+set :repository,  "git@github.com:netpenthe/sportspoo.git"
 
 set :user, "deployguy"
 default_run_options[:pty] = true
