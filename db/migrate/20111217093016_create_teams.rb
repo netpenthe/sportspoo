4adf983cede4e7cd5f2d420d1c7ceee13d7b0b23
nai2 <adnaismi@yahoo.com>
2013-11-03 22:10:26 +1030

    add index

--- db/migrate/20111217093016_create_teams.rb
+++ db/migrate/20111217093016_create_teams.rb
@@ -1,9 +1,14 @@
 class CreateTeams < ActiveRecord::Migration
+	
   def change
     create_table :teams do |t|
       t.string :name
       t.integer :bet_radar_id
       t.timestamps
     end
+
+    #CREATE INDEX idx_event_teams_id ON event_teams(event_id);
+    add_index :event_teams, :event_id
   end
+
 end
