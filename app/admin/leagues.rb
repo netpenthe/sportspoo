c82787069a82f9f22e61e142ff7dbbdb9b7f838e
nai2 <adnaismi@yahoo.com>
2013-11-17 19:44:52 +1030

    show league colours in leagues

--- app/admin/leagues.rb
+++ app/admin/leagues.rb
@@ -3,7 +3,9 @@ ActiveAdmin.register League do
   index do
     selectable_column
     id_column
-    column :name
+    column "name" do |league|
+    	raw "<span class='label' style='padding:1px 4px 2px;font-size:10px;-webkit-border-radius:3px;color:white;background-color:#{league.label_colour}'>#{league.name}</span>"
+    end
     default_actions
   end
 
