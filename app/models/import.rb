45e7ad9b020f7afa1f293f905a00706e2135d59c
nai2 <adnaismi@yahoo.com>
2015-02-08 13:26:37 +1030

    ics file type

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,10 +1,12 @@
 class Import < ActiveRecord::Base
+  
   attr_accessible :filter_out_summary, :league_name, :split_summary_on, :sport_name,:home_team_first
 
   attr_accessible :ics
   has_attached_file :ics
 
-  
+  do_not_validate_attachment_file_type :ics
+
   def import_events_count
     self.import_events.count
   end
